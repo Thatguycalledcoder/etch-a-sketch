63ede545be2692200e27af87ba61e5b836b98c8f
Thatguycalledcoder <quarshiedavid62@gmail.com>
2023-10-04 03:40:54 -0500

    Added random colors for each div on hover. Added random colors with event listeners for each div.

--- index.js
+++ index.js
@@ -8,10 +8,20 @@ redrawDiv.className = "redraw-btn";
 redrawBtn.id = "redraw";
 redrawBtn.innerText = "Redraw";
 
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
 // Get user input
 const getUserInput = function() {
-    const numRows = parseInt(prompt("Enter the number of rows to display for the grid"));
-    const numColumns = parseInt(prompt("Enter the number of columns to display for the grid"));
+    let numRows = 0;
+    do {
+        numRows = parseInt(prompt("Enter the number of rows to display for the grid. (Note: Number of rows cannot exceed 100!)"));
+    } while (numRows > 100);
+
+    let numColumns = 0;
+    do {
+        numColumns = parseInt(prompt("Enter the number of columns to display for the grid. (Note: Number of columns cannot exceed 100!)"));
+    } while (numColumns > 100);
+    
     return [numRows, numColumns];
 }
 
@@ -26,6 +36,12 @@ const createGrid = function(numRows = 16, numColumns = 16) {
         for (let rowIndex = 0; rowIndex < numColumns; rowIndex++) {
             const rowDiv = document.createElement("div");
             rowDiv.classList.add("grid-row");
+            rowDiv.addEventListener("mouseenter", () => {
+                rowDiv.style["background-color"] = colors[Math.floor(Math.random() * colors.length)];
+            })
+            rowDiv.addEventListener("mouseleave", () => {
+                rowDiv.style["background-color"] = "transparent";
+            })
             columnDiv.appendChild(rowDiv);
         }
         doc.appendChild(columnDiv);
